e370b53be5e2457a8e7fa993d2473a487853b678
Alexei Matveev <alexei.matveev@gmail.com>
2020-08-28 21:45:50 +0200

    AM: datascript 1.0.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [datascript "0.18.7"]
+                 [datascript "1.0.1"]
                  [criterium "0.4.5"]]
   :main ^:skip-aot hello-datascript.core
   :target-path "target/%s"
