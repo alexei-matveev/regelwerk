144f354f06fd4a5cbf486e66ee64c8161e220789
Alexei Matveev <alexei.matveev@gmail.com>
2020-09-09 21:12:10 +0200

    AM: unused-example noop

--- src/regelwerk/core.clj
+++ src/regelwerk/core.clj
@@ -7,26 +7,24 @@
 ;; also
 ;; https://stackoverflow.com/questions/42457136/recursive-datalog-queries-for-datomic-really-slow
 (defn- unused-example [n]
-  (let [rules (quote
-               [[(follows ?a ?b)
+  (let [rules '[[(follows ?a ?b)
                  [?a ?b]]
-                ;; Order of  clauses matters! Recursion in  1st or 2nd
-                ;; positon does not:
                 [(follows ?a ?b)
                  [?x ?b]
-                 (follows ?a ?x)]])
-        query (quote
-               [:find ?a ?b
+                 (follows ?a ?x)]]
+        query '[:find ?a ?b
                 :in $ %
                 :where
-                ;; [0 ?b] --- would be fast
-                (follows ?a ?b)
-                ;; [0 ?b] --- would be slow
-                ])
+                (follows ?a ?b)]
         ;; Looks like ([0 1] [1 2] [2 3]) for n = 3:
         db (for [i (range n)]
                [i (inc i)])]
     (d/q query db rules)))
+
+(comment
+  (unused-example 3)
+  =>
+  #{[2 3] [1 3] [0 3] [0 2] [1 2] [0 1]})
 
 (defn- parse [path]
   (edn/read (java.io.PushbackReader. (io/reader path))))
