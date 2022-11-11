feed57df93228e010f3c27c768010d4236b7feef
Alexei Matveev <alexei.matveev@gmail.com>
2022-11-11 23:12:55 +0100

    AM: (f a b)

--- src/regelwerk/lambda.clj
+++ src/regelwerk/lambda.clj
@@ -1,19 +1,24 @@
 (ns regelwerk.lambda
   (:require [datascript.core :as d]))
 
-;; Example query over two fact databases:
+;; Example query over two fact databases: find a variable that maybe 0
+;; in both databases. We dont need to declare parameters of a function
+;; [$a $b]  and refer to  the databases as  [$a $b] inside  the quoted
+;; query. But we can and do.
+(defn- f [$a $b]
+  (d/q '{:find [?v]
+         :in [$a $b]
+         :where [[$a ?v :eq 0]
+                 [$b ?v :eq 0]]}
+       $a
+       $b))
+
 (defn- binary []
-  (let [a [["x" :eq 0]
-           ["x" :eq 9]
-           ["y" :eq 0]]
-        b [["y" :eq 0]]]
-    ;; Find a variable that maybe 0 in both databases:
-    (d/q '{:find [?v]
-           :in [$a $b]
-           :where [[$a ?v :eq 0]
-                   [$b ?v :eq 0]]}
-         a
-         b)))
+  (let [a #{["x" :eq 0]
+            ["x" :eq 9]
+            ["y" :eq 0]}
+        b #{["y" :eq 0]}]
+    (f a b)))
 
 (binary) ;; => #{["y"]}
 
