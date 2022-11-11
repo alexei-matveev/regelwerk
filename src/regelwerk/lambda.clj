56e9e21b0e5990e84c98373182b48d9f5f114e9d
Alexei Matveev <alexei.matveev@gmail.com>
2022-11-11 23:05:35 +0100

    AM: rename vars

--- src/regelwerk/lambda.clj
+++ src/regelwerk/lambda.clj
@@ -3,17 +3,17 @@
 
 ;; Example query over two fact databases:
 (defn- binary []
-  (let [d1 [["a" :eq 0]
-            ["a" :eq 9]
-            ["b" :eq 0]]
-        d2 [["b" :eq 0]]]
+  (let [a [["x" :eq 0]
+           ["x" :eq 9]
+           ["y" :eq 0]]
+        b [["y" :eq 0]]]
     ;; Find a variable that maybe 0 in both databases:
-    (d/q '{:find [?x]
-           :in [$d1 $d2]
-           :where [[$d1 ?x :eq 0]
-                   [$d2 ?x :eq 0]]}
-         d1
-         d2)))
+    (d/q '{:find [?v]
+           :in [$a $b]
+           :where [[$a ?v :eq 0]
+                   [$b ?v :eq 0]]}
+         a
+         b)))
 
-;; (binary) => #{["b"]}
+(binary) ;; => #{["y"]}
 
