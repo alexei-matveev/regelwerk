b2cc25b755d8ffe8cfcdcc0f38e9988adde82eb0
Alexei Matveev <alexei.matveev@gmail.com>
2022-11-11 23:26:45 +0100

    AM: looks doable

--- src/regelwerk/lambda.clj
+++ src/regelwerk/lambda.clj
@@ -1,3 +1,5 @@
+;; It  is unclear  if  we  can extend  regelwerk.core  in a  backwards
+;; compatible way. So we make experiments in a separate namespace:
 (ns regelwerk.lambda
   (:require [datascript.core :as d]))
 
@@ -5,6 +7,10 @@
 ;; in both databases. We dont need to declare parameters of a function
 ;; [$a $b]  and refer to  the databases as  [$a $b] inside  the quoted
 ;; query. But we can and do.
+;;
+;; The function "f" ist a funciton of two datasets, $a and $b. But the
+;; declaration or definition of the  function involves a DS-Query with
+;; what we callt rule-variable ?v.
 (defn- f [$a $b]
   (d/q '{:find [?v]
          :in [$a $b]
@@ -12,6 +18,18 @@
                  [$b ?v :eq 0]]}
        $a
        $b))
+
+;; The  next evolution  of the  macro should  thus be  able to  take a
+;; Datalog  query with  an IN-clause  and define  the function  of the
+;; correspondig  number  of  datasets  like  above.   This  should  be
+;; functionally equivalent with the "defn" of "f" above:
+(comment
+  (defrules {:in [$a $b]
+             :find [?v]
+             :when [[$a ?v :eq 0]
+                    [$b ?v :eq 0]]
+             :then [[?v]]}))
+
 
 (defn- binary []
   (let [a #{["x" :eq 0]
