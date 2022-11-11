113fbccbbfddaba91e805b6000dd5fcd56a32ca1
Alexei Matveev <alexei.matveev@gmail.com>
2022-11-11 22:44:45 +0100

    AM: update clojure and datascript versions

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject f0bec0d/regelwerk "0.0.2"
+(defproject f0bec0d/regelwerk "0.0.3-SNAPSHOT"
   :description "Simplified rule engine"
   :url "https://github.com/alexei-matveev/regelwerk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
-                 [datascript "1.0.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [datascript "1.3.15"]]
   :main ^:skip-aot regelwerk.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
