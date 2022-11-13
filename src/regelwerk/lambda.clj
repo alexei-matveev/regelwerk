8f413e70247deea1646c0d159bec925fdd74cc42
Alexei Matveev <alexei.matveev@gmail.com>
2022-11-13 22:45:25 +0100

    AM: use macro from core.clj

--- src/regelwerk/lambda.clj
+++ src/regelwerk/lambda.clj
@@ -1,40 +1,6 @@
-;; It  is unclear  if  we  can extend  regelwerk.core  in a  backwards
-;; compatible way. So we make experiments in a separate namespace:
 (ns regelwerk.lambda
-  (:require [datascript.core :as d]))
-
-;; This will return the *code* of a  function for use in a macro or in
-;; an eval. Hence the name:
-(defn- compile-rule [rule]
-  ;; (:from rule) may be nil:
-  (let [{from :from, vars :find, expr :then, when :when} rule]
-    ;; It will be a function one  or more datasets.  As declared, this
-    ;; function  accepts arbitrary  number of  datasets.  However  the
-    ;; Datalog  query in  the body  may  complain about  "Too few"  or
-    ;; "Extra"  inputs  if   the  number  does  not   agree  with  the
-    ;; declarations in the IN/FROM-clause.
-    `(fn [& args#]
-       ;; Datascript appears to handle the case of nil for the
-       ;; IN-clause just OK:
-       (let [rows# (apply d/q
-                          '{:find ~vars, :in ~from, :where ~when}
-                          args#)]
-         ;; Generate another set of objects from the supplied collection
-         ;; valued  expression binding  each row  of the  result set  to
-         ;; variables  of a  vector.   Clojure indeed  allows binding  a
-         ;; vector of values to vector of  symbols --- a special case of
-         ;; "destructuring bind", so it is called, I think.
-         (into #{} cat     ; transducer works as (reduce into #{} ...)
-               (for [row# rows#]
-                 (let [~vars row#] ~expr)))))))
-
-;; Rule  is represented  by  a map  with at  least  find-, when-,  and
-;; then-clauses.   Rules involving  multiple  datasets  must supply  a
-;; from-clause too:
-(defmacro defrule [rule]
-  (compile-rule rule))
-
-;; The End. Only comments below this line ...
+  (:require [datascript.core :as d]
+            [regelwerk.core :as c]))
 
 ;; Motivation and thoughts ...
 (comment
@@ -95,19 +61,19 @@
 
 ;; Example usage for your C-u C-x C-e pleasure:
 (comment
-  (let [r1 (defrule {:from [$u]
-                     :find [?a ?b]
-                     :when [[$u ?a :is ?b]]
-                     :then [[?b :eq ?a]]})
-        r2 (defrule {:find [?a ?b]
-                     :when [[?a :is ?b]]
-                     :then [[?b :eq ?a]]})
-        r3 (defrule {:from [$u $v]
-                     :find [?x ?y]
-                     :when [[$u ?x :is ?z]
-                            [$v ?y :is ?z]]
-                     :then [[?x :le ?y]
-                            [?y :le ?x]]})
+  (let [r1 (c/defrule {:from [$u]
+                       :find [?a ?b]
+                       :when [[$u ?a :is ?b]]
+                       :then [[?b :eq ?a]]})
+        r2 (c/defrule {:find [?a ?b]
+                       :when [[?a :is ?b]]
+                       :then [[?b :eq ?a]]})
+        r3 (c/defrule {:from [$u $v]
+                       :find [?x ?y]
+                       :when [[$u ?x :is ?z]
+                              [$v ?y :is ?z]]
+                       :then [[?x :le ?y]
+                              [?y :le ?x]]})
         u [["a" :is 1]
            ["b" :is 2]]
         v [["A" :is 1]
